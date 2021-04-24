13b0007bbfac2431510b24ab3d16efe4ce264545
Naomi Kaufman <accounts@warducks.com>
2021-04-24 15:29:29 +0100

    FinalFloorTile

---
+++ BackGrounPracticeBas/Assets/Scripts/InteractableScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableScript : MonoBehaviour
+{
+
+    public Animator anim;
+    //Check If Enter
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("player");
+        }
+    }
+
+    
+}
